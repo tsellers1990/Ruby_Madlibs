86933d17ee64a0f70a07ff85593a836ee3011d99
tsellers1990 <tsellers1990@gmail.com>
2020-08-11 13:45:33 -0600

    initial push of the story to github

---
+++ madlibs.rb
@@ -0,0 +1,21 @@
+puts ("
+    {Name_1} and {Name_2}
+    Went up the hill
+    To fetch a pail of {Fluid_1}
+    {Name_1} {Action_1}
+    And broke his {Object_1},
+    And {Name_2} came tumbling after.
+
+    Up {Name_1} got
+    And {Place_1} did trot,
+    As fast as he could caper;
+    Went to bed
+    To mend his head
+    With {Fluid_2} and {Object_2}.
+
+    {Name_2} came in
+    And she did grin
+    To see his paper plaster;
+    Mother, vex’d,
+    Did whip her next
+    For causing {Name_1}'s disaster")
